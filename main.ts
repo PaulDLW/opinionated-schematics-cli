d154ca5381eb1b0e624a1a65bb166ad95005f656
Paul De La Warr <paul.delawarr@angloamerican.com>
2020-05-23 09:41:52 +0100

    fix: add tests

--- main.ts
+++ main.ts
@@ -1,15 +1,19 @@
 #!/usr/bin/env node
-import * as schematicCli from "@angular-devkit/schematics-cli/bin/schematics";
+import * as schematicCli from '@angular-devkit/schematics-cli/bin/schematics';
 
-const PACKAGE_NAME = "opinionated-schematics";
+const PACKAGE_NAME = 'opinionated-schematics';
 
-async function main() {
+export async function main() {
+  // ignore the first two arguments, they contain the path to the node exe
+  // and the path to the script file
   const args = process.argv.slice(2);
 
+  // grab the schematic name from the first arg, use the rest of the array as
+  // as parameters for the schematic
   const argsForNormalising = [`${PACKAGE_NAME}:${args[0]}`, ...args.slice(1)];
 
   const normalisedArgs = argsForNormalising.reduce(
-    (args, arg) => [...args, ...normaliseArg(arg)],
+    (normArgs, arg) => [...normArgs, ...normaliseArg(arg)],
     [] as string[]
   );
 
@@ -17,16 +21,16 @@ async function main() {
 }
 
 function normaliseArg(arg: string) {
-  if (arg.includes("=true")) {
-    const splitArg = arg.split("=") as any;
+  if (arg.includes('=true')) {
+    const splitArg = arg.split('=') as any;
     splitArg[1] = true;
     return splitArg;
-  } else if (arg.includes("=false")) {
-    const splitArg = arg.split("=") as any;
+  } else if (arg.includes('=false')) {
+    const splitArg = arg.split('=') as any;
     splitArg[1] = false;
     return splitArg;
   } else {
-    const normArg = arg === "true" ? true : arg === "false" ? false : arg;
+    const normArg = arg === 'true' ? true : arg === 'false' ? false : arg;
 
     return [normArg] as string[];
   }
